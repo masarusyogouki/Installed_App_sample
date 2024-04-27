a051e900af57de0cfbfd4e3e976036f956a18fdc
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-04-28 06:05:52 +0900

    非同期処理

--- app/src/main/java/com/example/installed_app_sample/viewmode/AppInfoViewModel.kt
+++ app/src/main/java/com/example/installed_app_sample/viewmode/AppInfoViewModel.kt
@@ -3,9 +3,12 @@ package com.example.installed_app_sample.viewmode
 import android.content.Context
 import android.content.Intent
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.installed_app_sample.model.AppInfo
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class AppInfoViewModel() : ViewModel() {
 
@@ -13,19 +16,21 @@ class AppInfoViewModel() : ViewModel() {
     val appInfoList: StateFlow<List<AppInfo>> = mutableappInfoList
 
     fun loadApps(context: Context) {
-        val pm = context.packageManager
-        val mainIntent = Intent(Intent.ACTION_MAIN, null)
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+        viewModelScope.launch(Dispatchers.IO) {
+            val pm = context.packageManager
+            val mainIntent = Intent(Intent.ACTION_MAIN, null)
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
 
-        val resolveInfoList = pm.queryIntentActivities(mainIntent, 0)
+            val resolveInfoList = pm.queryIntentActivities(mainIntent, 0)
 
-        val appInfos = resolveInfoList.map { resolveInfo ->
-            AppInfo(
-                name = resolveInfo.loadLabel(pm).toString(),
-                packageName = resolveInfo.activityInfo.packageName,
-                icon = resolveInfo.loadIcon(pm)
-            )
+            val appInfos = resolveInfoList.map { resolveInfo ->
+                AppInfo(
+                    name = resolveInfo.loadLabel(pm).toString(),
+                    packageName = resolveInfo.activityInfo.packageName,
+                    icon = resolveInfo.loadIcon(pm)
+                )
+            }
+            mutableappInfoList.value = appInfos
         }
-        mutableappInfoList.value = appInfos
     }
 }
